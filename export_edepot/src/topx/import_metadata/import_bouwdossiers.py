c6954b980af16c348e6f7b4a3e13956508ceb30a
Bart Heupers <bart.heupers@gmail.com>
2020-01-31 17:22:40 +0100

    WIP

--- export_edepot/src/topx/import_metadata/import_bouwdossiers.py
+++ export_edepot/src/topx/import_metadata/import_bouwdossiers.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+from jinja2 import Environment, PackageLoader
 import logging
-import re
 import os
+from pathlib import Path
+import shutil
+import sys
 import xmltodict
 
 log = logging.getLogger(__name__)
@@ -18,29 +21,66 @@ def get_list_items(d, key1, key2):
     return []
 
 
-def import_bouwdossiers(dir: str, file: str, dest_dir: str):
-    file_path = dir + os.sep + file
+def find_first_xml_file(dms_dir:str):
+    dms_xml_file = None
+    for file in os.listdir(dms_dir):
+        if file.endswith(".xml"):
+            dms_xml_file = file
+            break
+
+    return dms_xml_file
+
+
+def import_bouwdossiers(env, dms_dir: str, dest_dir: str):
+    dms_xml_file = find_first_xml_file(dms_dir)
+    if not dms_xml_file:
+        log.error('Geen XML gevonden in {dms_dir}')
+        return
+
+    if os.path.isdir(dest_dir):
+        shutil.rmtree(dest_dir)
+    Path(dest_dir).mkdir(parents=True, exist_ok=True)
+
+    file_path = dms_dir + os.sep + dms_xml_file
     with open(file_path) as fd:
         xml = xmltodict.parse(fd.read())
 
+    file_base = os.path.splitext(dms_xml_file)[0]
     xml_top = xml.get('overdracht_bouwdossier')
     overdracht_bouwdossier = {
-        'id': xml_top['@id'],
-        'classid' : xml_top['@classid'],
-        'soort' : xml_top['@soort'],
-        'datumpublicatie': datetime.fromisoformat(xml_top['datumpublicatie']['@valuecode']),
-        'omschrijvingpublicatie': xml_top['omschrijvingpublicatie']['#text'],
-    }
-    print (overdracht_bouwdossier)
+        'identificatiekenmerk': xml_top['@id'],
+        'naam': xml_top['omschrijvingpublicatie']['#text'],
+        'omschrijving': '',
+        'classificatie' : {
+            'code': xml_top['@classid'],
+            'omschrijving': xml_top['action'],  # ??
+            'datum': datetime.fromisoformat(xml_top['datumpublicatie']['@valuecode']).strftime("%Y-%m-%d"),  # ???
+            'bron': xml_top['@soort'],  # ??
 
+        },
+    }
+    template = env.get_template('ToPX-2.3_Archief_max.xml.jinja')
+    archief_xml = template.render(data=overdracht_bouwdossier)
+    archief_xml_path = dest_dir + os.sep + overdracht_bouwdossier['identificatiekenmerk'] + '.xml'
+    with open(archief_xml_path, "w") as output:
+        output.write(archief_xml)
+    # print (overdracht_bouwdossier)
     for bouwdossier in get_list_items(xml_top, 'bouwdossiers', 'bouwdossier'):
         print(f"oId:{bouwdossier.get('@oId')}")
+        for bijlage in get_list_items(bouwdossier, 'bijlagen', 'bijlage'):
+            document = bijlage['document']['#text']
+            bijlage_path = os.path.join(dms_dir, document)
+            if not os.path.exists(bijlage_path):
+                print(f"Missing : {bijlage_path}")
+            # else:
+            #    print(bijlage_path)
 
 
 if __name__ == "__main__":
     dms_dir = '/Users/bart/tmp/NHA/Eerste overdracht 1875 - 1900'
-    dms_xml_file = '000000636885001374942024.xml'
-    import_bouwdossiers(dms_dir, dms_xml_file, 'sidecar')
+    dest_dir = dms_dir + '.sidecar'
+    env = Environment(loader=PackageLoader('topx', 'templates'), trim_blocks=True, lstrip_blocks=True)
+    import_bouwdossiers(env, dms_dir, dest_dir)
 
 
 
--- export_edepot/src/validate.py
+++ export_edepot/src/validate.py
@@ -1,6 +1,8 @@
 from lxml import etree
 
-file = 'examples/NL-HlmNHA_test.wav.metadata'
+#file = 'examples/NL-HlmNHA_test.wav.metadata'
+
+file = '/Users/bart/tmp/NHA/Eerste overdracht 1875 - 1900.sidecar/0CD1B556-27B2-4BBE-8023-EE153CA0C70C.xml'
 # file = "examples/1498.metadata"
 
 schema_root = etree.parse("xsd/topx-2.3_1.xsd")
