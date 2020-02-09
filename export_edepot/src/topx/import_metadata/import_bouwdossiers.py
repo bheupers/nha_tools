d1557ddce371a8be6fa52740382b1d1d3e495327
Bart Heupers <bart.heupers@gmail.com>
2020-02-09 20:59:03 +0100

    WIP...

--- export_edepot/src/topx/import_metadata/import_bouwdossiers.py
+++ export_edepot/src/topx/import_metadata/import_bouwdossiers.py
@@ -1,13 +1,30 @@
 from datetime import datetime
 from jinja2 import Environment, PackageLoader
+import hashlib
 import logging
 import os
 from pathlib import Path
 import shutil
-import sys
 import xmltodict
 
+
 log = logging.getLogger(__name__)
+
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def sha256(fname):
+    sha256_hash = hashlib.sha256()
+    with open(fname,"rb") as f:
+        for chunk in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(chunk)
+    return sha256_hash.hexdigest()
 
 
 def get_list_items(d, key1, key2):
@@ -30,8 +47,45 @@ def find_first_xml_file(dms_dir:str):
 
     return dms_xml_file
 
+def get_string_adres(adres: dict):
+    straatnaam = adres['adresstraatnaam'].get('#text','')
+    aanduiding = adres['adresaanduiding'].get('#text','')
+    huisnummer = adres['adreshuisnummer'].get('#text','')
+    huisletter = adres['adreshuisletter'].get('#text','')
+    huisnummertoevoeging = adres['adreshuisnummertoevoeging'].get('#text','')
+    postcode = adres['adrespostcode'].get('#text','')
+    woonplaats = adres['adreswoonplaats'].get('#text','')
+
+    res = ''
+    if aanduiding:
+        res += aanduiding + ' '
+    if straatnaam:
+        res += straatnaam
+    if huisnummer:
+        res += ' ' + huisnummer
+    if huisletter:
+        res += huisletter
+    if huisnummertoevoeging:
+        res += ' ' + huisnummertoevoeging
+    if postcode or woonplaats:
+        res += ','
+    if postcode:
+        res += ' ' + postcode
+    if woonplaats:
+        res += ' ' + woonplaats
+    return res
+
 
 def import_bouwdossiers(env, dms_dir: str, dest_dir: str):
+    """
+    TopX structuur
+
+    Archief
+    Dossier
+    Bestand
+
+    Geen Serie of Record??
+    """
     dms_xml_file = find_first_xml_file(dms_dir)
     if not dms_xml_file:
         log.error('Geen XML gevonden in {dms_dir}')
@@ -47,8 +101,9 @@ def import_bouwdossiers(env, dms_dir: str, dest_dir: str):
 
     file_base = os.path.splitext(dms_xml_file)[0]
     xml_top = xml.get('overdracht_bouwdossier')
-    overdracht_bouwdossier = {
-        'identificatiekenmerk': xml_top['@id'],
+    identificatiekenmerk = xml_top['@id']
+    archief_data = {
+        'identificatiekenmerk': identificatiekenmerk,
         'naam': xml_top['omschrijvingpublicatie']['#text'],
         'omschrijving': '',
         'classificatie' : {
@@ -59,21 +114,91 @@ def import_bouwdossiers(env, dms_dir: str, dest_dir: str):
 
         },
     }
-    template = env.get_template('ToPX-2.3_Archief_max.xml.jinja')
-    archief_xml = template.render(data=overdracht_bouwdossier)
-    archief_xml_path = dest_dir + os.sep + overdracht_bouwdossier['identificatiekenmerk'] + '.xml'
-    with open(archief_xml_path, "w") as output:
-        output.write(archief_xml)
+    archief_template = env.get_template('ToPX-2.3_Archief_max.xml.jinja')
+    archief_metadata = archief_template.render(data=archief_data)
+    archief_metadata_path = dest_dir + os.sep + identificatiekenmerk + '.metadata'
+    with open(archief_metadata_path, "w") as output:
+        output.write(archief_metadata)
     # print (overdracht_bouwdossier)
     for bouwdossier in get_list_items(xml_top, 'bouwdossiers', 'bouwdossier'):
-        print(f"oId:{bouwdossier.get('@oId')}")
+        dossier_id = bouwdossier.get('@oId')
+        # print(f"oId:{dossier_id}")
+        dossier_template = env.get_template('ToPX-2.3_Dossier_max.xml.jinja')
+        bouwdossier_data = {
+            'identificatiekenmerk': dossier_id,
+            'naam': bouwdossier['dossiernummer']['#text'],  # ?? what is naam
+            'omschrijving': bouwdossier['omschrijving']['#text'],
+        }
+        bouwdosjaar = bouwdossier.get('bouwdosjaar', {}).get('#text')
+        dekking = None
+        if bouwdosjaar:
+            dekking = {
+                'intijd': {
+                    'begin': {
+                        'jaar': bouwdosjaar
+                    },
+                    'eind': {
+                        'jaar': bouwdosjaar
+                    }
+                }
+            }
+        adressen = [get_string_adres(adres) for adres in get_list_items(bouwdossier, 'adressen', 'adres')]
+        if adressen:
+            dekking['geografischgebied'] = {
+                'adressen': adressen
+            }
+        if dekking:
+            bouwdossier_data['dekking'] = dekking
+
+        dossier_meta_data = dossier_template.render(data=bouwdossier_data)
+        dossier_dest_dir = dest_dir + os.sep + identificatiekenmerk + os.sep
+        dossier_metadata_path = dossier_dest_dir + dossier_id + '.metadata'
+        Path(dossier_dest_dir).mkdir(parents=True, exist_ok=True)
+        with open(dossier_metadata_path, "w") as output:
+            output.write(dossier_meta_data)
+        bestand_dest_dir = dossier_dest_dir + dossier_id + os.sep
+        Path(bestand_dest_dir).mkdir(parents=True, exist_ok=True)
         for bijlage in get_list_items(bouwdossier, 'bijlagen', 'bijlage'):
+            bestand_id = bijlage['@oId']
             document = bijlage['document']['#text']
+            onderwerp = bijlage.get('onderwerp', {}).get('#text', '')
             bijlage_path = os.path.join(dms_dir, document)
+            statinfo = os.stat(bijlage_path)
+            bestand_size = statinfo.st_size
+            if bestand_size == 0:
+                log.error(f"Bestand with size 0 : {document}")
+                continue
+            sha256value = sha256(bijlage_path)
+
             if not os.path.exists(bijlage_path):
-                print(f"Missing : {bijlage_path}")
-            # else:
-            #    print(bijlage_path)
+                log.error(f"Missing : {bijlage_path}")
+                continue
+            shutil.copyfile(bijlage_path, os.path.join(bestand_dest_dir, document))
+            basename, extension = os.path.splitext(document)
+            bestand_data = {
+                'identificatiekenmerk': bestand_id,
+                'naam' : document,
+                'omschrijving': onderwerp,
+                'vorm': 1,
+                'formaat' : {
+                    'identificatiekenmerk' : bestand_id,  # formaat identificatiekenmerk ????
+                    'bestandsnaam' : {
+                        'naam' : basename,
+                        'extensie': extension
+                    },
+                    'omvang': bestand_size,
+                    'fysiekeintegriteit' : {
+                        'algoritme' : 'sha256',
+                        'waarde': sha256value,
+                        'datumentijd': datetime.now().isoformat()
+                    }
+                }
+            }
+            bestand_template = env.get_template('ToPX-2.3_Bestand_max.xml.jinja')
+            bestand_meta_data = bestand_template.render(data=bestand_data)
+            bestand_metadata_path = bestand_dest_dir + document + '.metadata'
+            with open(bestand_metadata_path, "w") as output:
+                output.write(bestand_meta_data)
 
 
 if __name__ == "__main__":
--- export_edepot/src/validate.py
+++ export_edepot/src/validate.py
@@ -2,9 +2,13 @@ from lxml import etree
 
 #file = 'examples/NL-HlmNHA_test.wav.metadata'
 
-file = '/Users/bart/tmp/NHA/Eerste overdracht 1875 - 1900.sidecar/0CD1B556-27B2-4BBE-8023-EE153CA0C70C.xml'
-# file = "examples/1498.metadata"
+# file = '/Users/bart/tmp/NHA/Eerste overdracht 1875 - 1900.sidecar/0CD1B556-27B2-4BBE-8023-EE153CA0C70C.metadata'
+# file = '/Users/bart/tmp/NHA/Eerste overdracht 1875 - 1900.sidecar/0CD1B556-27B2-4BBE-8023-EE153CA0C70C/0A2DE623-9AAD-4188-B1E7-B380BF2C3500.metadata'
 
+# file = "examples/1498.metadata"
+file = '/Users/bart/tmp/NHA/Eerste overdracht 1875 - 1900.sidecar/0CD1B556-27B2-4BBE-8023-EE153CA0C70C/0A2DE623-9AAD-4188-B1E7-B380BF2C3500/05e99f58-80d3-4d42-8248-d7b879ee4e59.tif.metadata'
+
+# file = '/Users/bart/Development/nha/nha_tools/export_edepot/src/topx/templates/ToPX-2.3_Bestand_max.xml'
 schema_root = etree.parse("xsd/topx-2.3_1.xsd")
 schema = etree.XMLSchema(schema_root)
 
